841bff9664c17070d9ad5ec7f87ea17855c4f490
Ubuntu <ubuntu@ip-172-31-25-124.ap-northeast-2.compute.internal>
2018-07-19 23:35:52 +0000

    feat(redux): init redux parts

--- target/src/index.js
+++ target/src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import ReactDom from "react-dom";
+import React from "react";
+import registerServiceWorker from './js/registerServiceWorker';
+import reducers from "./reducers";
+import App from './components/App';
+import './css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 스토어
+const store = createStore(reducers);
+ReactDom.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
+
 registerServiceWorker();
-console.log('public url: ', process.env.PUBLIC_URL)
